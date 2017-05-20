9332228a2b232e003838cb262bd5b563c29b0c65
William Charles <williamcharles@williamrlessmbp.attlocal.net>
2017-05-19 18:54:46 -0700

    assets/javascript/app.js index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,47 @@
+
+
+
+
+
 var sports =["football", "basketball", "baseball", "soccer", "tennis"];
 
 function displaySports() {
-	var sport = $(this).attr("data-name");
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" funny+cat "&api_key=dc6zaTOxFJmzC";
-
+	var sport = $(this).attr("data-sport");
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=dc6zaTOxFJmzC&limit=10";
+	
 	$.ajax({
-		queryURL: queryURL,
-		meghod: "GET"
-	}).done(function(response) {
+		url: queryURL,
+		method: "GET"
+	})
+
+	.done(function(response) {
+		console.log(queryURL);
 		console.log(response);
+		var results = response.data; 
+		for (var j=0; j<=10; j++) {
+			
+			$("<button>");
+			var sportDiv = $("<div>");
+			var p = $("<p>").text("Rating: " + results[j].rating);
+            var sportImage = $("<img>");
+            sportImage.attr("data-state", "still");            
+            sportImage.attr("src", results[j].images.fixed_height.url);
+            sportImage.attr("data-still", results[j].images.fixed_height_still.url);
+            sportImage.attr("data-animate", results[j].images.fixed_height_downsampled.url);
+            sportDiv.append(p);
+            sportDiv.append(sportImage);
+            $("#gifs").prepend(sportDiv);
+
+		}
 	});
 }
 
-displaySports();
-
-function addButtons {
+function addButtons() {
 	$("#buttons-view").empty();
 	for (var i = 0; i<sports.length; i++) {
-		var a =$("button");
+		var a = $("<button>");
 		a.addClass("sport");
-		a.attr("data-name", sports[i]);
+		a.attr("data-sport", sports[i]);
 		a.text(sports[i]);
 		$("#buttons-view").append(a);
 	}
@@ -27,14 +49,13 @@ function addButtons {
 
 $("#add-sport").on("click", function(event) {
 	event.preventDefault();
-	var sport = $("#button-input").val().trim();
+	var sportInput = $("#button-input").val().trim();
 
-	sports.push(sport);
-	console.log(sports);
+	sports.push(sportInput);
 
 	addButtons();
 });
 
-(document).on("click", ".sport", displaySports);
-      renderButtons();
+$(document).on("click", ".sport", displaySports);
+      addButtons();
 
